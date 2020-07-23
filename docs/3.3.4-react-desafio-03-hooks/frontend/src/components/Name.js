1f0ac0e6740118b8ed854113a5ce3ea5b29a3952
Daniel <danielgutk@gmail.com>
2020-07-23 13:28:23 -0300

    react-desafio-03 removed

--- docs/3.3.4-react-desafio-03-hooks/frontend/src/components/Name.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Name({ children }) {
-  return (
-    <div className="card-title blue-grey-text text-lighten-5">
-      {children}
-    </div>
-  )
-}
